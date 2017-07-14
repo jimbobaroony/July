1b72a7aee93bafa7e7132aa0cc7667384d9f783b
jimbobaroony <jamesedgargascoigne@gmail.com>
2017-07-14 21:47:38 +0100

    Add files via upload

---
+++ stackcreate20.json.js
@@ -0,0 +1,182 @@
+{
+  "Resources": {
+      "ProjectXNodeSecurityGroup" : {
+      "Type" : "AWS::EC2::SecurityGroup",
+      "Properties" : {
+        "GroupDescription" : "Enable SSH access and HTTP access on the inbound port",
+        "SecurityGroupIngress" :
+          [ { "IpProtocol" : "tcp", "FromPort" : "22", "ToPort" : "22", "CidrIp" : "0.0.0.0/0" },
+            { "IpProtocol" : "tcp", "FromPort" : "80", "ToPort" : "80", "CidrIp" : "0.0.0.0/0" }
+          ]
+      }
+   },
+    "CloudFormationASGroup" : {
+    "Type": "AWS::AutoScaling::AutoScalingGroup",
+      "Properties": {
+        "AvailabilityZones": [
+          "eu-west-1a"
+        ],
+        "Cooldown": "300",
+        "DesiredCapacity": "2",
+        "HealthCheckGracePeriod": "300",
+        "HealthCheckType": "EC2",
+        "MaxSize": "5",
+        "MinSize": "1",
+         "LaunchConfigurationName": {
+          "Ref": "lcCloudwatchLoggingLaunchConfig"
+        },
+        "Tags": [
+          {
+            "Key": "Cwgroup",
+            "Value": "Test",
+            "PropagateAtLaunch": "true"
+          }
+        ],
+        "TerminationPolicies": [
+          "Default"
+        ]
+      }
+    },
+    "lcCloudwatchLoggingLaunchConfig": {
+      "Type": "AWS::AutoScaling::LaunchConfiguration",
+      "Properties": {
+        "ImageId": "ami-fa411789",
+        "InstanceType": "t2.micro",
+        "KeyName": "cloudwatch",
+        "IamInstanceProfile": "cli-cloudwatch",
+        "SecurityGroups" : [{ "Ref" : "ProjectXNodeSecurityGroup" }],
+        "BlockDeviceMappings": [
+          {
+            "DeviceName": "/dev/xvda",
+            "Ebs": {
+              "VolumeSize": 8
+            }
+          }
+        ]
+      }
+    },
+"scalingHIGHCPU" : {
+  "Type" : "AWS::AutoScaling::ScalingPolicy",
+  "Properties" : {
+    "AdjustmentType" : "ChangeInCapacity",
+    "AutoScalingGroupName" : { "Ref" : "CloudFormationASGroup" },
+    "PolicyType" : "StepScaling",
+    "MetricAggregationType" : "Average",
+    "EstimatedInstanceWarmup" : "60",
+    "StepAdjustments": [
+      {
+        "MetricIntervalLowerBound": "0",
+        "MetricIntervalUpperBound" : "50",
+        "ScalingAdjustment": "1"
+      },
+      {
+        "MetricIntervalLowerBound": "50",
+        "ScalingAdjustment": "2"
+      }
+    ]
+  }
+},
+
+"scalingHIGHRAM" : {
+  "Type" : "AWS::AutoScaling::ScalingPolicy",
+  "Properties" : {
+    "AdjustmentType" : "ChangeInCapacity",
+    "AutoScalingGroupName" : { "Ref" : "CloudFormationASGroup" },
+    "PolicyType" : "StepScaling",
+    "MetricAggregationType" : "Average",
+    "EstimatedInstanceWarmup" : "60",
+    "StepAdjustments": [
+      {
+        "MetricIntervalLowerBound": "0",
+        "MetricIntervalUpperBound" : "50",
+        "ScalingAdjustment": "1"
+      },
+      {
+        "MetricIntervalLowerBound": "50",
+        "ScalingAdjustment": "2"
+      }
+    ]
+  }
+},
+
+
+"scalingHIGHNW" : {
+  "Type" : "AWS::AutoScaling::ScalingPolicy",
+  "Properties" : {
+    "AdjustmentType" : "ChangeInCapacity",
+    "AutoScalingGroupName" : { "Ref" : "CloudFormationASGroup" },
+    "PolicyType" : "StepScaling",
+    "MetricAggregationType" : "Average",
+    "EstimatedInstanceWarmup" : "60",
+    "StepAdjustments": [
+      {
+        "MetricIntervalLowerBound": "0",
+        "MetricIntervalUpperBound" : "50000000.0",
+        "ScalingAdjustment": "1"
+      },
+      {
+        "MetricIntervalLowerBound": "50000000.0",
+        "ScalingAdjustment": "2"
+      }
+    ]
+  }
+},
+    "alarmHIGHCPUASGROUP": {
+      "Type": "AWS::CloudWatch::Alarm",
+      "Properties": {
+        "ActionsEnabled": "true",
+        "ComparisonOperator": "GreaterThanOrEqualToThreshold",
+        "EvaluationPeriods": "1",
+        "MetricName": "CPUUtilization",
+        "Namespace": "AWS/EC2",
+        "Period": "300",
+        "Statistic": "Average",
+        "Threshold": "50.0",
+        "AlarmActions": [
+          {
+            "Ref": "scalingHIGHCPU"
+          },
+          "arn:aws:sns:eu-west-1:339623878616:Notify"
+        ]
+      }
+    },
+    "alarmHIGHNWASGROUP": {
+      "Type": "AWS::CloudWatch::Alarm",
+      "Properties": {
+        "ActionsEnabled": "true",
+        "ComparisonOperator": "GreaterThanOrEqualToThreshold",
+        "EvaluationPeriods": "1",
+        "MetricName": "NetworkIn",
+        "Namespace": "AWS/EC2",
+        "Period": "300",
+        "Statistic": "Average",
+        "Threshold": "50000000.0",
+        "AlarmActions": [
+          {
+            "Ref": "scalingHIGHNW"
+          },
+          "arn:aws:sns:eu-west-1:339623878616:Notify"
+        ]
+      }
+    },
+    "alarmHIGHRAMASGROUP": {
+      "Type": "AWS::CloudWatch::Alarm",
+      "Properties": {
+        "ActionsEnabled": "true",
+        "ComparisonOperator": "GreaterThanOrEqualToThreshold",
+        "EvaluationPeriods": "1",
+        "MetricName": "MemoryUtilization",
+        "Namespace": "System/Linux",
+        "Period": "300",
+        "Statistic": "Average",
+        "Threshold": "50.0",
+        "AlarmActions": [
+          {
+            "Ref": "scalingHIGHRAM"
+          },
+          "arn:aws:sns:eu-west-1:339623878616:Notify"
+        ],
+      }
+    }
+}
+}
